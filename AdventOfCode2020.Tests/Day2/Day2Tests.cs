a394b0e4dbc73cf8e1a8abec452224d32bd77bcd
mdavies <mdavies2@webroot.com>
2020-12-02 10:25:33 +0000

    Day 2

---
+++ AdventOfCode2020.Tests/Day2/Day2Tests.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using Xunit;
+using Xunit.Abstractions;
+using AdventOfCode2020.Day2;
+
+namespace AdventOfCode2020.Tests.Day2
+{
+    public class Day2Tests
+    {
+        private readonly ITestOutputHelper _testOutputHelper;
+
+        public Day2Tests(ITestOutputHelper testOutputHelper) 
+            => _testOutputHelper = testOutputHelper;
+        
+        [Fact]
+        public void GivenPolicyLineThenPolicyGetPopulatedAsExpected()
+        {
+            const string policyString = "1-3 a";
+            var (min, max, character) = PolicyParser.ParsePolicy(policyString);
+            
+            Assert.Equal(1, min);
+            Assert.Equal(3, max);
+            Assert.Equal("a", character);
+        }
+
+        [InlineData("1-3 a: abcde", true)]
+        [InlineData("1-3 b: cdefg", false)]
+        [InlineData("2-9 c: ccccccccc", true)]
+        [Theory]
+        public void GivenPolicy_ThenPasswordValidIsAsExpected(string policyString, bool expectedValidValue)
+        {
+            var (policy, password) = LineParser.ParseLine(policyString);
+
+            var isValid = policy.IsPasswordValid(password);
+            Assert.Equal(expectedValidValue, isValid);
+        }
+
+        [Fact]
+        public void SolvePuzzle1()
+        {
+            var exampleInput = new FileReader()
+                .GetResource("AdventOfCode2020.Tests.Day2.PuzzleInput.txt");
+
+            var count = 0;
+            var policyLines = exampleInput.Split(Environment.NewLine);
+            
+            foreach (var line in policyLines)
+            {
+                var (policy, password) = LineParser.ParseLine(line);
+                var isValid = policy.IsPasswordValid(password);
+                if (isValid)
+                    count++;
+            }
+
+            _testOutputHelper.WriteLine(count.ToString());
+        }
+        
+        [InlineData("1-3 a: abcde", true)]
+        [InlineData("1-3 b: cdefg", false)]
+        [InlineData("2-9 c: ccccccccc", false)]
+        [Theory]
+        public void GivenTobogganPolicy_ThenPasswordValidIsAsExpected(string policyString, bool expectedValidValue)
+        {
+            var (policy, password) = LineParser.ParseTobogganLine(policyString);
+
+            var isValid = policy.IsPasswordValid(password);
+            Assert.Equal(expectedValidValue, isValid);
+        }
+        
+        [Fact]
+        public void SolvePuzzle2()
+        {
+            var exampleInput = new FileReader()
+                .GetResource("AdventOfCode2020.Tests.Day2.PuzzleInput.txt");
+
+            var count = 0;
+            var policyLines = exampleInput.Split(Environment.NewLine);
+            
+            foreach (var line in policyLines)
+            {
+                var (policy, password) = LineParser.ParseTobogganLine(line);
+                var isValid = policy.IsPasswordValid(password);
+                if (isValid)
+                    count++;
+            }
+
+            _testOutputHelper.WriteLine(count.ToString());
+        }
+    }
+}
---
+++ AdventOfCode2020/Day2/PolicyParser.cs
@@ -0,0 +1,77 @@
+﻿using System.Linq;
+
+namespace AdventOfCode2020.Day2
+{
+    public static class PolicyParser
+    {
+        public static Policy ParsePolicy(string policyString)
+        {
+            var policy = policyString.Split(" ");
+
+            var minMax = policy[0].Split("-");
+            
+            var min = int.Parse(minMax[0]);
+            var max = int.Parse(minMax[^1]);
+            
+            var character = policy[1];
+            
+            return new Policy(min, max, character);
+        }
+        
+        public static TobogganPolicy ParseTobogganPolicy(string policyString)
+        {
+            var policy = policyString.Split(" ");
+
+            var positions = policy[0].Split("-");
+            
+            var firstPosition = int.Parse(positions[0]);
+            var secondPosition = int.Parse(positions[^1]);
+            
+            var character = policy[1];
+            
+            return new TobogganPolicy(firstPosition, secondPosition, character);
+        }
+    }
+
+    public static class LineParser
+    {
+        public static LineInformation ParseLine(string policyString)
+        {
+            var strings = policyString.Split(":");
+            var password = strings[^1].Trim();
+            return new(PolicyParser.ParsePolicy(strings[0]), password);
+        }
+        
+        public static LineInformation ParseTobogganLine(string policyString)
+        {
+            var strings = policyString.Split(":");
+            var password = strings[^1].Trim();
+            return new(PolicyParser.ParseTobogganPolicy(strings[0]), password);
+        }
+    }
+
+    public record LineInformation(Policy Policy, string Password);
+
+    public record Policy(int Min, int Max, string Character)
+    {
+        public virtual bool IsPasswordValid(string password)
+        {
+            var count = password.Count(character => character == Character[0]);
+            return count >= Min && count <= Max;
+        }
+    }
+
+    public record TobogganPolicy(int FirstPosition, int SecondPosition, string Character)
+        : Policy(FirstPosition, SecondPosition, Character)
+    {
+        public override bool IsPasswordValid(string password)
+        {
+            // Subtracting one because no index 0
+            var firstChar = password[FirstPosition - 1];
+            var secondChar = password[SecondPosition - 1];
+            var toCompare = Character[0];
+            return firstChar == toCompare ^ secondChar == toCompare;
+        }
+        
+    }
+}
