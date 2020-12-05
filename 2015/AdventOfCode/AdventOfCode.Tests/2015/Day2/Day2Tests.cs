ce098d55975cce55ab14ca013f5e7fc00bb6e740
Mark <markdavies550@gmail.com>
2020-12-05 21:56:33 +0000

    2015 day 2

---
+++ 2015/AdventOfCode/AdventOfCode.Tests/2015/Day2/Day2Tests.cs
@@ -0,0 +1,70 @@
+﻿using Xunit;
+using System;
+using System.Linq;
+using Xunit.Abstractions;
+using AdventOfCode._2015.Day2;
+
+namespace AdventOfCode.Tests._2015.Day2
+{
+    public class Day2Tests
+    {
+        private readonly ITestOutputHelper _testOutputHelper;
+
+        public Day2Tests(ITestOutputHelper testOutputHelper) 
+            => _testOutputHelper = testOutputHelper;
+
+        [InlineData("2x3x4", 52, 6)]
+        [InlineData("1x1x10", 42, 1)]
+        [Theory]
+        public void GivenRectangle_SurfaceAreaIsAsExpected(string input, int expectedSurfaceArea, int expectedSlack)
+        {
+            var record = RecordParser.Parse(input);
+            var (exactMeasurement, slack) = record.ConvertToWrappingPaper();
+
+            Assert.Equal(expectedSlack, slack);
+            Assert.Equal(expectedSurfaceArea, exactMeasurement);
+        }
+
+        [Fact]
+        public void SolvePuzzle1()
+        {
+            var input = FileReader.GetResource("AdventOfCode.Tests._2015.Day2.PuzzleInput.txt");
+            var sum = input.Split(Environment.NewLine)
+                .Select(RecordParser.Parse)
+                .Select(rect => rect.ConvertToWrappingPaper())
+                .Select(wp => wp.Total())
+                .Sum();
+            
+            _testOutputHelper.WriteLine(sum.ToString());
+        }
+
+        [InlineData("2x3x4", 10, 24)]
+        [InlineData("1x1x10", 4, 10)]
+        [Theory]
+        public void GivenRectangle_RibbonLengthIsAsExpected(string input, int expectedRibbonLength, int expectedBowLength)
+        {
+            var record = RecordParser.Parse(input);
+            var ribbon = record.ConvertToRibbon();
+
+            Assert.Equal(expectedRibbonLength, ribbon.ExactLength);
+            Assert.Equal(expectedBowLength, ribbon.BowLength);
+            Assert.Equal(expectedRibbonLength + expectedBowLength, ribbon.Total());
+        }
+
+        [Fact]
+        public void SolvePuzzle2()
+        {
+            var input = FileReader.GetResource("AdventOfCode.Tests._2015.Day2.PuzzleInput.txt");
+            var sum = input.Split(Environment.NewLine);
+                
+                
+                
+            var t = sum.Select(RecordParser.Parse);
+            var s = t.Select(rect => rect.ConvertToRibbon());
+            var u = s.Select(r => r.Total());
+            var v = u.Sum();
+            
+            _testOutputHelper.WriteLine(v.ToString());
+        }
+    }
+}
---
+++ 2015/AdventOfCode/AdventOfCode/2015/Day2/Rectangle.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+
+namespace AdventOfCode._2015.Day2
+{
+    public static class RecordParser
+    {
+        public static Rectangle Parse(string input)
+        {
+            var values = input.Split("x");
+            return new Rectangle(int.Parse(values[0]), int.Parse(values[1]), int.Parse(values[2]));
+        }
+    }
+    
+    public record Rectangle(int Length, int Width, int Height)
+    {
+        public WrappingPaper ConvertToWrappingPaper()
+        {
+            var values = new[]
+            {
+               Length * Width,
+               Width * Height,
+               Height * Length
+            };
+
+            var exactMeasurement = values.Aggregate(0, (c, v) => c + 2 * v);
+            return new WrappingPaper(exactMeasurement, values.Min());
+        }
+        
+        public Ribbon ConvertToRibbon()
+        {
+            var perimeters = new[]
+            {
+                Length + Length + Width + Width,
+                Width + Width + Height + Height,
+                Length + Length + Height + Height
+            };
+
+            return new(perimeters.Min(), Length * Width * Height);
+        }
+    }
+
+    public record WrappingPaper(int ExactMeasurement, int Slack)
+    {
+        public int Total() 
+            => ExactMeasurement + Slack;
+    }
+
+    public record Ribbon(int ExactLength, int BowLength)
+    {
+        public int Total() 
+            => ExactLength + BowLength;
+    }
+}
