5b9c152843e4c5f974201a4aaaf5f863e0a27dcb
Mark <markdavies550@gmail.com>
2020-12-06 21:17:30 +0000

    day 3 - 2015

---
+++ 2015/AdventOfCode/AdventOfCode.Tests/2015/Day3/Day3Tests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using AdventOfCode._2015.Day3;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace AdventOfCode.Tests._2015.Day3
+{
+    public class Day3Tests
+    {
+        private readonly ITestOutputHelper _testOutputHelper;
+
+        public Day3Tests(ITestOutputHelper testOutputHelper) 
+            => _testOutputHelper = testOutputHelper;
+
+        [Fact]
+        public void GivenOneRightDirection_ThenTwoPresentsAreGiven()
+        {
+            var houses = PresentParser.Parse(">");
+            Assert.Equal(2, houses.Count);
+        }
+        
+        [InlineData("^>v<", 4)]
+        [InlineData("^v^v^v^v^v", 2)]
+        [Theory]
+        public void GivenInput_ExpectedAmountOfHousesAreVisited(string input, int expectedVisitedHouses)
+        {
+            var houses = PresentParser.Parse(input);
+            Assert.Equal(expectedVisitedHouses, houses.Count);
+        }
+
+        [Fact]
+        public void SolvePuzzle1()
+        {
+            var input = FileReader.GetResource("AdventOfCode.Tests._2015.Day3.PuzzleInput.txt");
+            var houses = PresentParser.Parse(input);
+            _testOutputHelper.WriteLine(houses.Count.ToString());
+        }
+        
+        [InlineData("^v", 3)]
+        [InlineData("^>v<", 3)]
+        [InlineData("^v^v^v^v^v", 11)]
+        [Theory]
+        public void Puzzle2GivenInput_ExpectedAmountOfHousesAreVisited(string input, int expectedVisitedHouses)
+        {
+            var houses = PresentParser.ParsePuzzle2(input);
+            Assert.Equal(expectedVisitedHouses, houses.Count);
+        }
+
+        [Fact]
+        public void SolvePuzzle2()
+        {
+            var input = FileReader.GetResource("AdventOfCode.Tests._2015.Day3.PuzzleInput.txt");
+            var houses = PresentParser.ParsePuzzle2(input);
+            _testOutputHelper.WriteLine(houses.Count.ToString());
+            
+        }
+    }
+}
---
+++ 2015/AdventOfCode/AdventOfCode/2015/Day3/PresentParser.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode._2015.Day3
+{
+    public static class PresentParser
+    {
+        public static List<House> Parse(string input)
+        {
+            var houses = new List<House> {new House(0, 0)};
+
+            foreach (var house in 
+                from letter in input 
+                let lastHouse = houses[^1] 
+                select letter switch
+            {
+                '>' => lastHouse with { X = lastHouse.X + 1},
+                '<' => lastHouse with { X = lastHouse.X - 1},
+                '^' => lastHouse with { Y = lastHouse.Y + 1},
+                'v' => lastHouse with { Y = lastHouse.Y - 1},
+                _ => new House(0, 0)
+            })
+            {
+                houses.Add(house);
+            }
+
+            return houses.Distinct()
+                .ToList();
+        }
+        
+        public static List<House> ParsePuzzle2(string input)
+        {
+            var initialHouse = new House(0, 0);
+            
+            
+            var houses = new List<House> {initialHouse};
+
+            for (var i = 0; i < input.Length; i ++)
+            {
+                houses.Add(i == 0 ? HandleCharacter(houses[^1], input[i]) : HandleCharacter(houses[^2], input[i]));
+            }
+
+            
+            
+            
+            return houses.Distinct()
+                .ToList();
+        }
+
+        private static House HandleCharacter(House house, char letter) =>
+            letter switch
+            {
+                '>' => house with { X = house.X + 1},
+                '<' => house with { X = house.X - 1},
+                '^' => house with { Y = house.Y + 1},
+                'v' => house with { Y = house.Y - 1},
+                _ => new House(0, 0)
+            };
+    }
+
+    public record House(int X, int Y);
+}
