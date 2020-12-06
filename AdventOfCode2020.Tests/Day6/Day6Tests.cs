fd44b5bbb0d06874f18b22d2eaed25f308115e2f
Mark <markdavies550@gmail.com>
2020-12-06 10:48:30 +0000

    Day6

---
+++ AdventOfCode2020.Tests/Day6/Day6Tests.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using Xunit;
+using System.Linq;
+using AdventOfCode2020.Day6;
+using Xunit.Abstractions;
+
+namespace AdventOfCode2020.Tests.Day6
+{
+    public class Day6Tests
+    {
+        private readonly ITestOutputHelper _testOutputHelper;
+
+        public Day6Tests(ITestOutputHelper testOutputHelper) 
+            => _testOutputHelper = testOutputHelper;
+
+        [Fact]
+        public void GivenAGroup_WithThreeUniqueAnswers_ThenCountIsThree()
+        {
+            const string input = "abc";
+            var groups = AnswerParser.Parse(input);
+
+            var sum = groups.Select(g => g.GetGroupAnswerCount())
+                .Sum();
+            Assert.Equal(3, sum);
+        }
+
+        [Fact]
+        public void GivenTwoGroupsOfThreeYesAnswers_ThenTotalIsSix()
+        {
+            const string input = @"abc
+
+a
+b
+c";
+            var groups = AnswerParser.Parse(input);
+
+            var sum = groups.Select(g => g.GetGroupAnswerCount())
+                .Sum();
+            Assert.Equal(6, sum);
+        }
+
+        [Fact]
+        public void SolvePuzzle1()
+        {
+            var puzzleInput = new FileReader()
+                .GetResource("AdventOfCode2020.Tests.Day6.PuzzleInput.txt");
+            var groups = AnswerParser.Parse(puzzleInput);
+            var sum = groups.Select(g => g.GetGroupAnswerCount())
+                .Sum();
+            _testOutputHelper.WriteLine(sum.ToString());
+        }
+        
+        [Fact]
+        public void Puzzle2_GivenAGroup_WithThreeUniqueAnswers_ThenCountIsThree()
+        {
+            const string input = "abc";
+            var groups = AnswerParser.Parse(input);
+
+            var sum = groups.Select(g => g.GetGroupAnswerCount())
+                .Sum();
+            Assert.Equal(3, sum);
+        }
+
+        [Fact]
+        public void Puzzle2_GivenTwoGroupsOfThreeYesAnswers_ThenTotalIsThree()
+        {
+            const string input = @"abc
+
+a
+b
+c";
+            var groups = AnswerParser.ParsePuzzle2(input);
+
+            var sum = groups.Select(g => g.GetGroupAnswerCount())
+                .Sum();
+            Assert.Equal(3, sum);
+        }
+
+        [Fact]
+        public void Puzzle2_ExampleInput()
+        {
+            const string input = @"abc
+
+a
+b
+c
+
+ab
+ac
+
+a
+a
+a
+a
+
+b";
+            var groups = AnswerParser.ParsePuzzle2(input);
+
+            var sum = groups.Select(g => g.GetGroupAnswerCount())
+                .Sum();
+            Assert.Equal(6, sum);
+        }
+        
+        [Fact]
+        public void SolvePuzzle2()
+        {
+            var puzzleInput = new FileReader()
+                .GetResource("AdventOfCode2020.Tests.Day6.PuzzleInput.txt");
+            var groups = AnswerParser.ParsePuzzle2(puzzleInput);
+            var sum = groups.Select(g => g.GetGroupAnswerCount())
+                .Sum();
+            _testOutputHelper.WriteLine(sum.ToString());
+        }
+    }
+}
---
+++ AdventOfCode2020/Day6/AnswerParser.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace AdventOfCode2020.Day6
+{
+    public static class AnswerParser
+    {
+        public static IEnumerable<Group> Parse(string input) =>
+            input.Split(Environment.NewLine + Environment.NewLine)
+                .Select(group => string.Join("", group.Split(Environment.NewLine)))
+                .Select(answers => new Group(answers)).ToList();
+
+
+        public static IEnumerable<GroupAnswer> ParsePuzzle2(string input) =>
+            input.Split(Environment.NewLine + Environment.NewLine)
+                .Select(group => new GroupAnswer(group.Split(Environment.NewLine).ToList()));
+    }
+
+    public record GroupAnswer(List<string> Answers)
+    {
+        public int GetGroupAnswerCount()
+        {
+            return Answers.Count == 1
+                ? Answers[0].Length
+                : Answers[0].Count(character => Answers.Skip(1).All(a => a.Contains(character)));
+        }
+    }
+
+    public record Group(string Answers)
+    {
+        public int GetGroupAnswerCount() 
+            => Answers.Distinct().Count();
+    }
+}
