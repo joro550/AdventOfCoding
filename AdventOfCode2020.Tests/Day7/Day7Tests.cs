ec970168811eb995c0e565cf81ec64c651018e62
mdavies <mdavies2@webroot.com>
2020-12-07 10:39:21 +0000

    Day 7 - will come back to this

---
+++ AdventOfCode2020.Tests/Day7/Day7Tests.cs
@@ -0,0 +1,46 @@
+﻿using Xunit;
+using Xunit.Abstractions;
+using AdventOfCode2020.Day7;
+
+namespace AdventOfCode2020.Tests.Day7
+{
+    public class Day7Tests
+    {
+        private readonly ITestOutputHelper _testOutputHelper;
+
+        public Day7Tests(ITestOutputHelper testOutputHelper) 
+            => _testOutputHelper = testOutputHelper;
+
+        [Fact]
+        public void ExamplePuzzle()
+        {
+            const string input = @"light red bags contain 1 bright white bag, 2 muted yellow bags.
+dark orange bags contain 3 bright white bags, 4 muted yellow bags.
+bright white bags contain 1 shiny gold bag.
+muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
+shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
+dark olive bags contain 3 faded blue bags, 4 dotted black bags.
+vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
+faded blue bags contain no other bags.
+dotted black bags contain no other bags.";
+
+            var bags = BagParser.Parse(input);
+            var traverser = new Traverser(bags);
+            var count = traverser.FindThing("shiny", "gold");
+            Assert.Equal(4, count);
+        }
+        
+        [Fact]
+        public void SolvePuzzle1()
+        {
+            var puzzleInput = new FileReader()
+                .GetResource("AdventOfCode2020.Tests.Day7.PuzzleInput.txt");
+
+            var bags = BagParser.Parse(puzzleInput);
+            var traverser = new Traverser(bags);
+            var count = traverser.FindThing("shiny", "gold");
+
+            _testOutputHelper.WriteLine(count.ToString());
+        }
+    }
+}
---
+++ AdventOfCode2020/Day7/BagParser.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode2020.Day7
+{
+    public static class BagParser
+    {
+        public static List<Bag> Parse(string input)
+        {
+            var bagList = new List<Bag>();
+
+            foreach (var line in input.Split(Environment.NewLine))
+            {
+                var parts = line.Split("contain");
+                var bag = Bag.FromString(parts[0].Split(" "));
+
+                foreach (var rules in parts[1].Split(","))
+                {
+                    var words = rules.Split(" ").Where(s => !string.IsNullOrEmpty(s))
+                        .ToArray();
+
+                    var didParse = int.TryParse(words[0], out var num);
+                    var number = didParse ? num : 0;
+                    var bagRule = Bag.FromString(words[1..]);
+
+                    bag.BagRule.Add(new(number, bagRule));
+                }
+
+                bagList.Add(bag);
+            }
+
+            return bagList;
+        }
+    }
+
+    public class Traverser
+    {
+        private readonly List<Bag> _bags;
+
+        public Traverser(List<Bag> bags) 
+            => _bags = bags;
+
+        public int FindThing(string modifier, string colour) 
+            => FindInBagRule(modifier, colour, _bags).Count;
+
+        private HashSet<string> FindInBagRule(string modifier, string colour, IEnumerable<Bag> bags)
+        {
+            var hashSet = new HashSet<string>();
+            foreach (var bag in bags)
+            {
+                var rule = _bags.SingleOrDefault(b => b.Modifier == bag.Modifier && b.Colour == bag.Colour);
+                if(rule == null)
+                    continue;
+                
+                var canHoldBag = rule.BagRule.Any(r => r.bag.Modifier == modifier && r.bag.Colour == colour);
+
+                if (canHoldBag)
+                {
+                    hashSet.Add(rule.GetName());
+                }
+                else
+                {
+                    var bagRule = FindInBagRule(modifier, colour, bag.BagRule.Select(r => r.bag));
+                    if(bagRule.Any())
+                        hashSet.Add(bag.GetName());
+                }
+            }
+
+            return hashSet;
+        }
+    }
+    
+    public record Bag(string Modifier, string Colour)
+    {
+        public readonly List<(int Number, Bag bag)> BagRule =
+            new();
+
+        public static Bag FromString(string[] input) 
+            => new(input[0], input[1]);
+
+        public string GetName()
+            => $"{Modifier} {Colour}";
+    }
+}
