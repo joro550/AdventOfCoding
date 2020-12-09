c5c3bcc723834fae4cc0cdad3c23fdedb58c6896
Mark <markdavies550@gmail.com>
2020-12-09 08:14:23 +0000

    Day9

---
+++ AdventOfCode2020.Tests/Day9/Day9Tests.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using AdventOfCode2020.Day9;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace AdventOfCode2020.Tests.Day9
+{
+    public class Day9Tests
+    {
+        private readonly ITestOutputHelper _testOutputHelper;
+
+        public Day9Tests(ITestOutputHelper testOutputHelper) 
+            => _testOutputHelper = testOutputHelper;
+
+        [Fact]
+        public void GivenExampleInput_ThenExpectedNumberIsNotASumOfPreviousNumbers()
+        {
+            var xmas = XmasParser.Parse(5, @"35
+20
+15
+25
+47
+40
+62
+55
+65
+95
+102
+117
+150
+182
+127
+219
+299
+277
+309
+576");
+            var i = 0;
+            while (xmas.Check(i)) 
+                i++;
+
+            Assert.Equal(127, xmas.GetNumberAtIndex(i));
+
+        }
+
+        [Fact]
+        public void SolvePuzzle1()
+        {
+            var puzzleInput = new FileReader()
+                .GetResource("AdventOfCode2020.Tests.Day9.PuzzleInput.txt");
+            var xmas = XmasParser.Parse(25, puzzleInput);
+            
+            var i = 0;
+            while (xmas.Check(i)) 
+                i++;
+            
+            Assert.Equal(1212510616,xmas.GetNumberAtIndex(i) );
+        }
+
+        [Fact]
+        public void Example2()
+        {
+            var xmas = XmasParser.Parse(5, @"35
+20
+15
+25
+47
+40
+62
+55
+65
+95
+102
+117
+150
+182
+127
+219
+299
+277
+309
+576");
+            var sum = xmas.FindContiguomSumFor(127);
+            Assert.Equal(62, sum);
+
+        }
+        
+        
+        [Fact]
+        public void SolvePuzzle2()
+        {
+            var puzzleInput = new FileReader()
+                .GetResource("AdventOfCode2020.Tests.Day9.PuzzleInput.txt");
+            var xmas = XmasParser.Parse(25, puzzleInput);
+            
+            var sum = xmas.FindContiguomSumFor(1212510616);
+            Assert.Equal(171265123, sum);
+        }
+    }
+}
---
+++ AdventOfCode2020/Day9/XmasParser.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace AdventOfCode2020.Day9
+{
+    public static class XmasParser
+    {
+        public static Xmas Parse(int preambleLength, string input)
+        {
+            var numberList = input.Split(Environment.NewLine)
+                .Select(long.Parse)
+                .ToList();
+            return new Xmas(numberList, preambleLength);
+        }
+    }
+
+    public record Xmas
+    {
+        private readonly int _preambleLength;
+        private readonly List<long> _numbers;
+
+        public Xmas(IEnumerable<long> numbers, int preambleLength)
+        {
+            _preambleLength = preambleLength;
+            _numbers = numbers.ToList();
+        }
+
+        public long GetNumberAtIndex(int index) 
+            => _numbers[index + _preambleLength];
+
+        public bool Check(int index)
+        {
+            var endOfRange = index + _preambleLength;
+            var preAmble = new List<long>();
+                
+            for (var i = index; i < endOfRange; i++) 
+                preAmble.Add(_numbers[i]);
+
+            var numberToMatch = _numbers[index + _preambleLength];
+
+            return preAmble.Where((t3, i) =>
+                (from t2 in preAmble.Where((t2, j) => i != j)
+                    let t = t3
+                    let t1 = t2
+                    where t + t1 == numberToMatch
+                    select t).Any()).Any();
+        }
+
+        public long FindContiguomSumFor(long number)
+        {
+            for (var i = 0; i < _numbers.Count; i++)
+            {
+                var numbers = new List<long> {_numbers[i]};
+                var result = Add(i, number, numbers);
+
+                if (result != number) 
+                    continue;
+                
+                var min = numbers.Min();
+                var max = numbers.Max();
+                return min + max;
+            }
+
+            return 0;
+        }
+
+        private long Add(int startingNumber, long aimingFor, ICollection<long> currentNumbers)
+        {
+            var index = startingNumber + 1;
+            currentNumbers.Add(_numbers[index]);
+            
+            var sum = currentNumbers.Sum();
+            if (sum == aimingFor)
+            {
+                return aimingFor;
+            }
+
+            return sum < aimingFor ? 
+                Add(index, aimingFor, currentNumbers) 
+                : 0;
+        }
+    }
+}
