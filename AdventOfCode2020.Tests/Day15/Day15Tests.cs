ebbc5a4afd2d5fda12833de091e6527ce99d7f14
mdavies <mdavies2@webroot.com>
2020-12-15 10:41:02 +0000

    Day 15 starting out

---
+++ AdventOfCode2020.Tests/Day15/Day15Tests.cs
@@ -0,0 +1,34 @@
+﻿using AdventOfCode2020.Day15;
+using Xunit;
+
+namespace AdventOfCode2020.Tests.Day15
+{
+    public class Day15Tests
+    {
+        [Fact]
+        public void Example1()
+        {
+            var counter = new NumberCounter();
+            var number = counter.GetNumber("0,3,6", 10);
+            Assert.Equal(0, number);
+            
+            counter = new NumberCounter();
+            number = counter.GetNumber("0,3,6", 2020);            
+            Assert.Equal(436, number);
+        }
+
+        [Theory]
+        [InlineData("1,3,2", 1)]
+        [InlineData("2,1,3", 10)]
+        [InlineData("1,2,3", 27)]
+        [InlineData("2,3,1", 78)]
+        [InlineData("3,2,1", 438)]
+        [InlineData("3,1,2", 1836)]
+        public void FirstPuzzleExamples(string input, long expectedOutput)
+        {
+            var counter = new NumberCounter();
+            var number = counter.GetNumber(input, 2020);
+            Assert.Equal(expectedOutput, number);
+        }
+    }
+}
---
+++ AdventOfCode2020/Day15/NumberCounter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode2020.Day15
+{
+    public class NumberCounter
+    {
+        public readonly Dictionary<int, int> _numbers = new();
+        
+        
+        private readonly HashSet<(int Number, int Index)> _hashSet
+            = new();
+
+        public long GetNumber(string numbers, int index)
+        {
+            var startingNumbers = numbers.Split(",").Select(int.Parse).ToArray();
+
+            for (var i = 0; i < startingNumbers.Length - 1; i++) 
+                _numbers.Add(startingNumbers[i], i + 1);            
+
+            var lastNumberSpoken = startingNumbers[^1];
+            for (var i = startingNumbers.Length; i < index; i++)
+            {
+                var containsKey = _numbers.ContainsKey(lastNumberSpoken);
+                if (containsKey)
+                {
+                    lastNumberSpoken = i - _numbers[lastNumberSpoken];
+                    _numbers[lastNumberSpoken] = i + 1;
+                }
+                else
+                {
+                    _numbers.Add(lastNumberSpoken, i);
+                    lastNumberSpoken = 0;
+                }
+            }
+
+            return lastNumberSpoken;
+        }
+    }
+}
