89b0a2c15d78027ceb56a82e708506799097816b
mdavies <mdavies2@webroot.com>
2020-12-15 13:06:55 +0000

    Day 15 done

--- AdventOfCode2020.Tests/Day15/Day15Tests.cs
+++ AdventOfCode2020.Tests/Day15/Day15Tests.cs
@@ -11,10 +11,6 @@ namespace AdventOfCode2020.Tests.Day15
             var counter = new NumberCounter();
             var number = counter.GetNumber("0,3,6", 10);
             Assert.Equal(0, number);
-            
-            counter = new NumberCounter();
-            number = counter.GetNumber("0,3,6", 2020);            
-            Assert.Equal(436, number);
         }
 
         [Theory]
@@ -23,12 +19,29 @@ namespace AdventOfCode2020.Tests.Day15
         [InlineData("1,2,3", 27)]
         [InlineData("2,3,1", 78)]
         [InlineData("3,2,1", 438)]
+        [InlineData("0,3,6", 436)]
         [InlineData("3,1,2", 1836)]
         public void FirstPuzzleExamples(string input, long expectedOutput)
         {
             var counter = new NumberCounter();
             var number = counter.GetNumber(input, 2020);
             Assert.Equal(expectedOutput, number);
+        }
+
+        [Fact]
+        public void SolvePuzzle1()
+        {
+            var counter = new NumberCounter();
+            var number = counter.GetNumber("9,19,1,6,0,5,4", 2020);
+            Assert.Equal(1522, number);
+        }
+        
+        [Fact]
+        public void SolvePuzzle2()
+        {
+            var counter = new NumberCounter();
+            var number = counter.GetNumber("9,19,1,6,0,5,4", 30000000);
+            Assert.Equal(18234, number);
         }
     }
 }
--- AdventOfCode2020/Day15/NumberCounter.cs
+++ AdventOfCode2020/Day15/NumberCounter.cs
@@ -1,41 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 
 namespace AdventOfCode2020.Day15
 {
     public class NumberCounter
     {
-        public readonly Dictionary<int, int> _numbers = new();
+        private readonly Dictionary<int, int> _numbers = new();
         
-        
-        private readonly HashSet<(int Number, int Index)> _hashSet
-            = new();
-
         public long GetNumber(string numbers, int index)
         {
             var startingNumbers = numbers.Split(",").Select(int.Parse).ToArray();
 
             for (var i = 0; i < startingNumbers.Length - 1; i++) 
-                _numbers.Add(startingNumbers[i], i + 1);            
+                _numbers.Add(startingNumbers[i], i + 1);
 
-            var lastNumberSpoken = startingNumbers[^1];
+            var nextNumber = startingNumbers[^1];
             for (var i = startingNumbers.Length; i < index; i++)
             {
-                var containsKey = _numbers.ContainsKey(lastNumberSpoken);
+                var containsKey = _numbers.ContainsKey(nextNumber);
                 if (containsKey)
                 {
-                    lastNumberSpoken = i - _numbers[lastNumberSpoken];
-                    _numbers[lastNumberSpoken] = i + 1;
+                    var previousNumber = nextNumber;
+                    nextNumber = i - _numbers[previousNumber];
+                    _numbers[previousNumber] = i;
                 }
                 else
                 {
-                    _numbers.Add(lastNumberSpoken, i);
-                    lastNumberSpoken = 0;
+                    _numbers.Add(nextNumber, i);
+                    nextNumber = 0;
                 }
             }
-
-            return lastNumberSpoken;
+            return nextNumber;
         }
     }
 }
