18601246fe5e95f350e6a3d8205ccd4ddd6109d0
mdavies <mdavies2@webroot.com>
2020-12-16 11:23:56 +0000

    Day 16

---
+++ AdventOfCode2020.Tests/Day16/Day16Tests.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AdventOfCode2020.Day16;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace AdventOfCode2020.Tests.Day16
+{
+    public class Day16Tests
+    {
+        private readonly ITestOutputHelper _testOutputHelper;
+
+        public Day16Tests(ITestOutputHelper testOutputHelper)
+        {
+            _testOutputHelper = testOutputHelper;
+        }
+
+        [Fact]
+        public void Example1()
+        {
+            var input = @"class: 1-3 or 5-7
+row: 6-11 or 33-44
+seat: 13-40 or 45-50
+
+your ticket:
+7,1,14
+
+nearby tickets:
+7,3,47
+40,4,50
+55,2,20
+38,6,12";
+
+            var tickets = TicketParser.Parse(input);
+            var invalidFields = new List<long>();
+
+            foreach (var ticket in tickets.NearbyTickets)
+            {
+                foreach (var field in ticket.Fields)
+                {
+                    if (tickets.Fields.All(f => !f.Validate(field)))
+                    {
+                        invalidFields.Add(field);
+                    }
+                }
+            }
+
+            Assert.Equal(71, invalidFields.Sum());
+        }
+        
+        
+        [Fact]
+        public void Puzzle2Example()
+        {
+            var input = @"class: 0-1 or 4-19
+row: 0-5 or 8-19
+seat: 0-13 or 16-19
+
+your ticket:
+11,12,13
+
+nearby tickets:
+3,9,18
+15,1,5
+5,14,9";
+            
+            var tickets = TicketParser.Parse(input);
+
+            foreach (var ticket in from ticket in tickets.NearbyTickets 
+                from field in ticket.Fields 
+                where tickets.Fields.All(f => !f.Validate(field)) 
+                select ticket)
+            {
+                ticket.IsValid = false;
+            }
+
+            var validTickets = tickets.NearbyTickets.Where(ticket => ticket.IsValid)
+                .ToArray();
+            
+            var fieldDictionary = new Dictionary<int, List<TicketField>>();
+            for (var i = 0; i < validTickets.First().Fields.Count; i++) 
+                fieldDictionary.Add(i, new List<TicketField>(tickets.Fields));
+
+            foreach (var ticket in validTickets)
+            {
+                for (int i = 0; i < ticket.Fields.Count; i++)
+                {
+                    for (var j = 0; j < fieldDictionary[i].Count; j++)
+                    {
+                        var rule = fieldDictionary[i][j];
+                        if (rule.Validate(ticket.Fields[i])) 
+                            continue;
+                        
+                        fieldDictionary[i].RemoveAt(j);
+                        j--;
+                    }
+                }
+            }
+            
+            while (fieldDictionary.Keys.Count != fieldDictionary.Select(d => d.Value.Count).Sum())
+            {
+                var namesToDelete = (from field in fieldDictionary 
+                        where field.Value.Count == 1 
+                        select field.Value.First().Name).ToList();
+                
+                var withMultipleEntry = fieldDictionary.Where(s => s.Value.Count != 1);
+
+                foreach (var entry in withMultipleEntry)
+                {
+                    for (int i = 0; i < entry.Value.Count; i++)
+                    {
+                        if (namesToDelete.All(a => a != entry.Value[i].Name)) 
+                            continue;
+                        
+                        entry.Value.RemoveAt(i);
+                        i--;
+                    }
+                }
+            }
+
+            for (int i = 0; i < validTickets.First().Fields.Count; i++)
+            {
+                var @join = string.Join(",", fieldDictionary[i].Select(field => field.Name));
+                _testOutputHelper.WriteLine($"{i} : {@join}");
+            }
+        }
+        
+        [Fact]
+        public void Puzzle2()
+        {
+            var input = new FileReader()
+                .GetResource("AdventOfCode2020.Tests.Day16.PuzzleInput.txt");
+            var tickets = TicketParser.Parse(input);
+
+            // foreach (var ticket in from ticket in tickets.NearbyTickets 
+            //     from field in ticket.Fields 
+            //     where tickets.Fields.All(f => !f.Validate(field)) 
+            //     select ticket)
+            // {
+            //     ticket.IsValid = false;
+            // }
+            //
+            // var validTickets = tickets.NearbyTickets.Where(ticket => ticket.IsValid)
+            //     .ToArray();
+            //
+            // var fieldDictionary = new Dictionary<int, List<TicketField>>();
+            // for (var i = 0; i < validTickets.First().Fields.Count; i++) 
+            //     fieldDictionary.Add(i, new List<TicketField>(tickets.Fields));
+            //
+            //
+            // foreach (var ticket in validTickets)
+            // {
+            //     for (int i = 0; i < ticket.Fields.Count; i++)
+            //     {
+            //         for (var j = 0; j < fieldDictionary[i].Count; j++)
+            //         {
+            //             var rule = fieldDictionary[i][j];
+            //             if (rule.Validate(ticket.Fields[i])) 
+            //                 continue;
+            //             
+            //             fieldDictionary[i].RemoveAt(j);
+            //             j--;
+            //         }
+            //     }
+            // }
+            //
+            // while (fieldDictionary.Keys.Count != fieldDictionary.Select(d => d.Value.Count).Sum())
+            // {
+            //     var namesToDelete = (from field in fieldDictionary 
+            //         where field.Value.Count == 1 
+            //         select field.Value.First().Name).ToList();
+            //     
+            //     var withMultipleEntry = fieldDictionary.Where(s => s.Value.Count != 1);
+            //
+            //     foreach (var entry in withMultipleEntry)
+            //     {
+            //         for (int i = 0; i < entry.Value.Count; i++)
+            //         {
+            //             if (namesToDelete.All(a => a != entry.Value[i].Name)) 
+            //                 continue;
+            //             
+            //             entry.Value.RemoveAt(i);
+            //             i--;
+            //         }
+            //     }
+            // }
+            //
+            // for (int i = 0; i < validTickets.First().Fields.Count; i++)
+            // {
+            //     var @join = string.Join(",", fieldDictionary[i].Select(field => field.Name));
+            //     _testOutputHelper.WriteLine($"{i} : {@join}");
+            // }
+            
+            /*
+                0 : departure date
+                1 : departure platform
+                2 : arrival station
+                3 : departure track
+                4 : row
+                5 : departure time
+                6 : train
+                7 : departure station
+                8 : duration
+                9 : class
+                10 : route
+                11 : arrival location
+                12 : zone
+                13 : arrival track
+                14 : seat
+                15 : wagon
+                16 : arrival platform
+                17 : type
+                18 : departure location
+                19 : price
+             */
+
+            var departureSum = tickets.MyTicket.Fields[0] *
+                               tickets.MyTicket.Fields[1] *
+                               tickets.MyTicket.Fields[3] *
+                               tickets.MyTicket.Fields[5] *
+                               tickets.MyTicket.Fields[7] *
+                               tickets.MyTicket.Fields[18];
+            _testOutputHelper.WriteLine(departureSum.ToString());
+        }
+    }
+}
---
+++ AdventOfCode2020/Day16/TicketParser.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace AdventOfCode2020.Day16
+{
+    public static class TicketParser
+    {
+        public static Thing Parse(string input)
+        {
+            var values = input.Split(Environment.NewLine + Environment.NewLine);
+            var rules = GetTicketRules(values[0]);
+            
+            var tickets = values[1].Split(Environment.NewLine)[1..];
+            var myTicket = ParseTicket(tickets, rules);
+            
+            tickets = values[2].Split(Environment.NewLine)[1..];
+            var nearbyTickets = ParseTicket(tickets, rules);
+
+            return new Thing
+            {
+                MyTicket = myTicket.First(), 
+                NearbyTickets = nearbyTickets, 
+                Fields = rules
+            };
+        }
+
+        private static List<Ticket> ParseTicket(IEnumerable<string> tickets, List<TicketField> fields)
+        {
+            var returnTickets = new List<Ticket>();
+            
+            foreach (var ticket in tickets)
+            {
+                var values = ticket.Split(",");
+                var ticketFields = new List<long>();
+                
+                for (int i = 0; i < values.Length; i++)
+                {
+                    // var ticketField = fields[i] with {Value = int.Parse(values[i])};
+                    ticketFields.Add(long.Parse(values[i]));
+                }
+
+                returnTickets.Add(new Ticket(ticketFields));
+            }
+            
+            return returnTickets;
+        }
+        
+
+        private static List<TicketField> GetTicketRules(string values)
+        {
+            var fields = new List<TicketField>();
+            foreach (var rule in values.Split(Environment.NewLine))
+            {
+                var sides = rule.Split(":");
+                var name = sides[0];
+
+                var rangeRule = new List<TicketRule>();
+                foreach (var range in sides[^1].Split("or"))
+                {
+                    var minMax = range.Split("-");
+                    
+                    var min = int.Parse(minMax[0].Trim());
+                    var max = int.Parse(minMax[1].Trim());
+                    rangeRule.Add(new RangeRule(min, max));
+                }
+                fields.Add(new TicketField(name, rangeRule));
+            }
+
+            return fields;
+        }
+    }
+
+    public record Thing
+    {
+        public Ticket MyTicket { get; init; }
+        public List<Ticket> NearbyTickets { get; init; }
+
+        public List<TicketField> Fields = new List<TicketField>();
+    }
+    
+    public record Ticket(List<long> Fields)
+    {
+        public bool IsValid { get; set; } = true;
+    }
+
+    //
+    // public record Ticket(List<TicketField> Fields)
+    // {
+    //     
+    // }
+
+    public record TicketField(string Name, List<TicketRule> Rules)
+    {
+        public int Value { get; init; }
+
+        public bool Validate(long value) 
+            => Rules.Any(rule => rule.Validate(value));
+    }
+
+    public record RangeRule(int Min, int Max) : TicketRule
+    {
+        public override bool Validate(long input) 
+            => input >= Min && input <= Max;
+    }
+
+    public abstract record TicketRule
+    {
+        public abstract bool Validate(long input);
+    }
+}
