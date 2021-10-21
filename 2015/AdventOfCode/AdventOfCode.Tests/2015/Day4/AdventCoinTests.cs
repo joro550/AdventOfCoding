4e614bc8ed611bc4c5755c6e84722af3d5402155
joro550 <markdavies550@gmail.com>
2021-10-21 13:13:58 +0100

    Puzzle 1 done

---
+++ 2015/AdventOfCode/AdventOfCode.Tests/2015/Day4/AdventCoinTests.cs
@@ -0,0 +1,48 @@
+﻿using Xunit;
+using AdventOfCode._2015.Day4;
+
+namespace AdventOfCode.Tests._2015.Day4
+{
+    public class AdventCoinTests
+    {
+        private CoinMiner _miner;
+
+        public AdventCoinTests() 
+            => _miner = new CoinMiner();
+
+        [Fact]
+        public void Example1()
+        {
+            const string key = "abcdef";
+            
+            var thing = _miner.GetValidCoinNumber(key);
+            Assert.Equal(609043, thing);
+        }
+        
+        [Fact]
+        public void Example2()
+        {
+            const string key = "pqrstuv";
+            var thing = _miner.GetValidCoinNumber(key);
+            Assert.Equal(1048970, thing);
+        }
+        
+        [Fact]
+        public void Puzzle1()
+        {
+            const string key = "iwrupvqb";
+            var thing = _miner.GetValidCoinNumber(key);
+            Assert.Equal(346386, thing);
+        }
+        
+        [Fact]
+        public void Puzzle2()
+        {
+            const string key = "iwrupvqb";
+
+            var coinMiner = new CoinMiner(6);
+            var thing = coinMiner.GetValidCoinNumber(key);
+            Assert.Equal(346386, thing);
+        }
+    }
+}
---
+++ 2015/AdventOfCode/AdventOfCode/2015/Day4/CoinMiner.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+
+namespace AdventOfCode._2015.Day4
+{
+    public class CoinMiner
+    {
+        private readonly int _numberOfZeros;
+
+        public CoinMiner(int numberOfZeros = 5) => _numberOfZeros = numberOfZeros;
+
+        public int GetValidCoinNumber(string key)
+        {
+            var hash = string.Empty;
+            using var md5Hasher = MD5.Create();
+            
+            int i = 1;
+            for (; !IsValidHash(hash); i++) 
+                hash = FromByteArray(md5Hasher.ComputeHash(ToByteArray(key, i)));
+
+            return i - 1;
+        }
+
+        private static byte[] ToByteArray(string key, int value)
+            => Encoding.ASCII.GetBytes($"{key}{value}"); 
+
+        private static string FromByteArray(byte[] data)
+        {
+            // var sb = new StringBuilder();
+            // foreach (var t in data) 
+            //     sb.Append(t.ToString("X2"));
+            // return sb.ToString();
+            return BitConverter.ToString(data).Replace("-","");
+        }
+
+        private bool IsValidHash(string hash) 
+            => hash.Length >= _numberOfZeros && "00000".Equals(hash[.._numberOfZeros], StringComparison.CurrentCultureIgnoreCase);
+    }
+}
