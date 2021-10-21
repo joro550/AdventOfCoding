6f7a6a537ff2d81b47760450ab0d446769eb4fd3
joro550 <markdavies550@gmail.com>
2021-10-21 14:56:43 +0100

    Day 5 - puzzle 1

---
+++ 2015/AdventOfCode/AdventOfCode.Tests/2015/Day5/LetterEvalRuleTests.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Linq;
+using AdventOfCode._2015.Day5;
+using Xunit;
+
+namespace AdventOfCode.Tests._2015.Day5
+{
+    public class LetterEvalRuleTests
+    {
+        [Fact]
+        public void WhenWordDoesntMatchRules_FalseIsReturned()
+        {
+            var rule = new LetterEvalRule(new[] { 'a', 'e', 'i', 'o', 'u' }, 3);
+            var isValid = rule.Evaluate("bad");
+            Assert.False(isValid);
+        } 
+        
+        [Fact]
+        public void WhenWordMatchesRules_TrueIsReturned()
+        {
+            var rule = new LetterEvalRule(new[] { 'a', 'e', 'i', 'o', 'u' }, 3);
+            var isValid = rule.Evaluate("aei");
+            Assert.True(isValid);
+        } 
+    }
+    
+    public class LetterDuplicationRuleTests
+    {
+        [Fact]
+        public void WhenWordDoesntMatchRules_FalseIsReturned()
+        {
+            var rule = new LetterDuplicationRule(2, 1);
+            var isValid = rule.Evaluate("bad");
+            Assert.False(isValid);
+        }
+        
+        [Theory]
+        [InlineData("baad")]
+        [InlineData("badd")]
+        [InlineData("bbad")]
+        public void WhenWordDoesMatchRules_TrueIsReturned(string word)
+        {
+            var rule = new LetterDuplicationRule(2, 1);
+            var isValid = rule.Evaluate(word);
+            Assert.True(isValid);
+        } 
+    }
+
+    public class WordExclusionTests
+    {
+        [Fact]
+        public void WhenWordDoesntMatchRules_FalseIsReturned()
+        {
+            var rule = new WordExclusion("aa");
+            var isValid = rule.Evaluate("baad");
+            Assert.False(isValid);
+        }
+        
+        [Fact]
+        public void WhenWordDoesMatchRules_TrueIsReturned()
+        {
+            var rule = new WordExclusion("aa");
+            var isValid = rule.Evaluate("bad");
+            Assert.True(isValid);
+        }
+    }
+
+    public class Puzzle1Tests
+    {
+        [Theory]
+        [InlineData("ugknbfddgicrmopn", true)]
+        [InlineData("aaa", true)]
+        [InlineData("jchzalrnumimnmhp", false)]
+        [InlineData("haegwjzuvuyypxyu", false)]
+        [InlineData("dvszwmarrgswjxmb", false)]
+        public void Example1(string word, bool expectedResult)
+        {
+            var evaluator = new WordEvaluator()
+            {
+                Rules =
+                {
+                    new LetterEvalRule(new[] { 'a', 'e', 'i', 'o', 'u' }, 3),
+                    new LetterDuplicationRule(2, 1),
+                    new WordExclusion("ab"),
+                    new WordExclusion("cd"),
+                    new WordExclusion("pq"),
+                    new WordExclusion("xy"),
+                }
+            };
+
+            var isNice = evaluator.IsNiceWord(word);
+            Assert.Equal(expectedResult, isNice);
+        }
+
+        [Fact]
+        public void Puzzle1()
+        {
+            var input = FileReader
+                .GetResource("AdventOfCode.Tests._2015.Day5.PuzzleInput.txt");
+
+            var evaluator = new WordEvaluator
+            {
+                Rules =
+                {
+                    new LetterEvalRule(new[] { 'a', 'e', 'i', 'o', 'u' }, 3),
+                    new LetterDuplicationRule(2, 1),
+                    new WordExclusion("ab"),
+                    new WordExclusion("cd"),
+                    new WordExclusion("pq"),
+                    new WordExclusion("xy"),
+                }
+            };
+
+            var count = input.Split(Environment.NewLine)
+                .Count(word => evaluator.IsNiceWord(word));
+            Assert.Equal(258, count);
+        }
+    }
+}
---
+++ 2015/AdventOfCode/AdventOfCode/2015/Day5/WordEvaluator.cs
@@ -0,0 +1,60 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+
+namespace AdventOfCode._2015.Day5
+{
+    public class WordEvaluator
+    {
+        public List<Rule> Rules { get; }
+            = new();
+
+        public bool IsNiceWord(string word) 
+            => Rules.All(rule => rule.Evaluate(word));
+    }
+
+    public record WordExclusion(string Word) : Rule
+    {
+        public override bool Evaluate(string word) 
+            => !word.Contains(Word);
+    }
+    
+    public record LetterDuplicationRule(int NumberOfLetters, int Times) : Rule
+    {
+        public override bool Evaluate(string word)
+        {
+            var queue = new Queue<char>(word);
+
+            var currentCount = 0;
+            var lastLetter = '\0';
+
+            while (queue.TryDequeue(out var letter))
+            {
+                if (letter == lastLetter)
+                {
+                    currentCount++;
+                    if (currentCount == Times)
+                        return true;
+                    continue;
+                }
+
+                lastLetter = letter;
+                currentCount = 0;
+            }
+            return false;
+        }
+    }
+
+    public record LetterEvalRule(char[] Letters, int Min) : Rule
+    {
+        public override bool Evaluate(string word)
+        {
+            var count = word.Count(letter => Letters.Any(x => x == letter));
+            return count >= Min;
+        }
+    }
+
+    public abstract record Rule
+    {
+        public abstract bool Evaluate(string word);
+    }
+}
