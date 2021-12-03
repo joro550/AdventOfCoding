16d8dd7632960b894a524cd98419beea0b20ee41
mdavies <mdavies2@webroot.com>
2021-12-03 15:11:44 +0000

    Day 3

---
+++ 2015/AdventOfCode/AdventOfCodeFsharp.Tests/2021_Day3.fs
@@ -0,0 +1,105 @@
+﻿module AdventOfCodeFsharp.Tests._2021_Day3
+
+open AdventOfCodeFsharp._2021.Day3
+open Xunit
+open AdventOfCodeFsharp.library
+
+
+[<Fact>]
+let ``Puzzle1`` () =
+    async {
+        let! fileContent = loadFileAsync("AdventOfCodeFsharp._2021.Day3.txt") |> Async.AwaitTask
+        let gamma = getGammaValue(fileContent)
+        let epsilon = getEpsilonRate(fileContent)
+        
+        Assert.Equal(1025636, gamma * epsilon)
+    }
+    
+[<Fact>]
+let ``Puzzle2`` () =
+    async {
+        let! fileContent = loadFileAsync("AdventOfCodeFsharp._2021.Day3.txt") |> Async.AwaitTask
+        let oxygen = getOxygenRating(fileContent)
+        let co2 = getco2Rating(fileContent)
+        
+        Assert.Equal(793873, oxygen * co2)
+    }
+    
+[<Fact>]
+let ``Given example then co2 is returned`` ()=
+    let value = "00100
+11110
+10110
+10111
+10101
+01111
+00111
+11100
+10000
+11001
+00010
+01010"
+    let oxygenRating = getco2Rating(value)
+    Assert.Equal(10, oxygenRating);
+    
+[<Fact>]
+let ``Given example then oxygen rating is returned`` ()=
+    let value = "00100
+11110
+10110
+10111
+10101
+01111
+00111
+11100
+10000
+11001
+00010
+01010"
+    let oxygenRating = getOxygenRating(value)
+    Assert.Equal(23, oxygenRating);
+
+
+let ``Given an string then array of ints is returned`` ()=
+    let value = "00100"
+    let arrayValue = stringToIntArray(value)
+    Assert.Equal(arrayValue[0], 0)
+    Assert.Equal(arrayValue[1], 0)
+    Assert.Equal(arrayValue[2], 1)
+    Assert.Equal(arrayValue[3], 0)
+    Assert.Equal(arrayValue[4], 0)
+     
+[<Fact>]
+let ``Given example then string is returned`` ()=
+    let value = "00100
+11110
+10110
+10111
+10101
+01111
+00111
+11100
+10000
+11001
+00010
+01010"
+    let answer = getGammaValue(value)
+    Assert.Equal(22, answer)
+    
+    
+[<Fact>]
+let ``Given example then epsilon is returned`` ()=
+    let value = "00100
+11110
+10110
+10111
+10101
+01111
+00111
+11100
+10000
+11001
+00010
+01010"
+    let answer = getEpsilonRate(value)
+    Assert.Equal(9, answer)
---
+++ 2015/AdventOfCode/AdventOfCodeFsharp/2021/Day3.fs
@@ -0,0 +1,66 @@
+﻿module AdventOfCodeFsharp._2021.Day3
+
+open System
+
+let stringToIntArray (input: string) =
+    input.ToCharArray()
+        |> Array.map (fun elem -> elem.ToString() |> Int32.Parse)
+                
+let getGammaValue (input:string)=
+    let length = input.Split(Environment.NewLine).Length /2
+    let value = input.Split(Environment.NewLine)
+                    |> Array.map (fun elem -> elem |> stringToIntArray)
+                    |> Array.reduce (fun acc elem -> elem |> Array.map2 (+) acc)
+                    |> Array.map (fun elem -> if elem > length then "1" else "0")
+                    |> String.concat ""
+        
+    Convert.ToInt32(value, 2)
+    
+    
+let getEpsilonRate (input:string)=
+    let length = input.Split(Environment.NewLine).Length /2
+    let value = input.Split(Environment.NewLine)
+                    |> Array.map (fun elem -> elem |> stringToIntArray)
+                    |> Array.reduce (fun acc elem -> elem |> Array.map2 (+) acc)
+                    |> Array.map (fun elem -> if elem > length then "0" else "1")
+                    |> String.concat ""
+        
+    Convert.ToInt32(value, 2)
+    
+let getMostPopularNumber (input:string[])=
+    let length = if input.Length % 2 > 0 then input.Length/2 + 1 else input.Length /2
+    input
+        |> Array.map (fun elem -> elem |> stringToIntArray)
+        |> Array.reduce (fun acc elem -> elem |> Array.map2 (+) acc)
+        |> Array.map (fun elem -> if elem >= length then "1" else "0")
+        
+let getLeastPopularNumber (input:string[])=
+    let length = if input.Length % 2 > 0 then input.Length/2 + 1 else input.Length /2
+    let thing = input
+                    |> Array.map (fun elem -> elem |> stringToIntArray)
+                    |> Array.reduce (fun acc elem -> elem |> Array.map2 (+) acc)
+    Array.map (fun elem -> if elem >= length then "0" else "1") thing
+
+let getOxygenRating (input:string)=
+    let mutable i =0
+    let mutable values = input.Split(Environment.NewLine)
+
+    
+    
+    while values.Length > 1 do
+        let mostPopular = getMostPopularNumber(values)
+        values <- values |> Array.filter (fun elem -> elem[i].ToString() = mostPopular[i])
+        i <- i + 1
+
+    Convert.ToInt32(values[0],2)
+    
+let getco2Rating (input:string)=
+    let mutable i =0
+    let mutable values = input.Split(Environment.NewLine)
+
+    while values.Length > 1 do
+        let mostPopular = getLeastPopularNumber(values)
+        values <- values |> Array.filter (fun elem -> elem[i].ToString() = mostPopular[i])
+        i <- i + 1
+
+    Convert.ToInt32(values[0],2)
