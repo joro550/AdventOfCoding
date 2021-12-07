c3dac3468f1720bc1f2d4b92c233418f52d3af40
mdavies <mdavies2@webroot.com>
2021-12-07 14:22:10 +0000

    Day 7

---
+++ 2015/AdventOfCode/AdventOfCodeFsharp.Tests/2021/2021_Day7.fs
@@ -0,0 +1,61 @@
+﻿module AdventOfCodeFsharp.Tests._2021.Day7
+
+open Xunit
+open AdventOfCodeFsharp._2021.Day7
+open AdventOfCodeFsharp.library
+
+[<Fact>]
+let ``Given input then submarines are returned`` ()=
+    let input = "16,1,2,0,4,2,7,1,2,14"
+    let submarines = getSubmarines(input)
+    Assert.Equal(16, submarines[0])
+    Assert.Equal(1, submarines[1])
+    Assert.Equal(2, submarines[2])
+    Assert.Equal(0, submarines[3])
+    Assert.Equal(4, submarines[4])
+    
+    
+[<Fact>]
+let ``Given position 2 then fuel expended is 37`` ()=
+    let input = "16,1,2,0,4,2,7,1,2,14"
+    let fuelCost = getSubmarines(input)
+                        |> (fun elem -> getFuelCost(elem, 2))
+    Assert.Equal(37, fuelCost)
+    
+    
+[<Fact>]
+let ``Get Fuel 2 Given position 5 then fuel expended is 168`` ()=
+    let input = "16,1,2,0,4,2,7,1,2,14"
+    let fuelCost = getSubmarines(input)
+                        |> (fun elem -> getFuelCost2(elem, 5))
+    Assert.Equal(168, fuelCost)
+    
+[<Fact>]
+let ``Example 1`` ()=
+    let input = "16,1,2,0,4,2,7,1,2,14"
+    let leastAmountOfFuel = getLeastAmountOfFuel(input)
+    Assert.Equal(37, leastAmountOfFuel)
+    
+    
+[<Fact>]
+let ``Example 2`` ()=
+    let input = "16,1,2,0,4,2,7,1,2,14"
+    let leastAmountOfFuel = getLeastAmountOfFuel2(input)
+    Assert.Equal(168, leastAmountOfFuel)
+    
+[<Fact>]
+let ``Puzzle 1`` ()=
+    async {
+        let! fileContent = loadFileAsync("AdventOfCodeFsharp._2021.Day7.txt") |> Async.AwaitTask
+        let leastAmountOfFuel = getLeastAmountOfFuel(fileContent)
+        Assert.Equal(340056, leastAmountOfFuel)
+    }
+    
+[<Fact>]
+let ``Puzzle 2`` ()=
+    async {
+        let! fileContent = loadFileAsync("AdventOfCodeFsharp._2021.Day7.txt") |> Async.AwaitTask
+        let leastAmountOfFuel = getLeastAmountOfFuel2(fileContent)
+        Assert.Equal(96592275, leastAmountOfFuel)
+    }
+    
---
+++ 2015/AdventOfCode/AdventOfCodeFsharp/2021/Day7.fs
@@ -0,0 +1,41 @@
+﻿module AdventOfCodeFsharp._2021.Day7
+
+let getSubmarines (input:string) =
+    input.Split(",")
+        |> Array.map int
+        
+let getFuelCost( input: int[], position :int) =
+     input
+            |> Array.map (fun elem ->
+                if elem > position then elem - position else position - elem)
+            |> Array.sum
+
+let getLeastAmountOfFuel (input:string) =
+    let submarines = input |> getSubmarines
+    
+    let mutable fuel : List<int> = []
+    for sub in submarines do
+        fuel <- [getFuelCost(submarines, sub)] |> List.append fuel
+    
+    fuel |> List.reduce (fun acc elem -> if acc < elem then acc else elem)
+    
+    
+let getArraySum(input: int) =
+    [| for i in 1 .. input -> i |]
+        |> Array.sum
+
+let getFuelCost2(input: int[], position :int) =
+     input
+            |> Array.map (fun elem ->
+                if elem > position then elem - position else position - elem)
+            |> Array.map (fun elem -> elem |> getArraySum)
+            |> Array.sum
+    
+let getLeastAmountOfFuel2 (input:string) =
+    let submarines = input |> getSubmarines
+    let maxPosition = submarines |> Array.max
+    let minPosition = submarines |> Array.min
+    
+    [for i = minPosition to maxPosition do getFuelCost2(submarines, i)]
+        |> List.reduce (fun acc elem -> if acc < elem then acc else elem)
+        
