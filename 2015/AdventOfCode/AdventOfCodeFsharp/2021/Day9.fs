3ec9cbf4c4efa60e037276011a8288025be71c99
mdavies <mdavies2@webroot.com>
2021-12-09 16:21:20 +0000

    Update

--- 2015/AdventOfCode/AdventOfCodeFsharp/2021/Day9.fs
+++ 2015/AdventOfCode/AdventOfCodeFsharp/2021/Day9.fs
@@ -14,17 +14,22 @@ type Map(things: Thing[][])=
     member _.getXLength ()= things[0].Length - 1
     
     member this.getNeighbourArray (x:int,y:int)=
-        if x = 0 && y = 0 then [| (x+1,y); (x,y+1)  |] // top left
-        else if x = this.getXLength() && y = 0 then [| (x-1, y); (x, y+1)  |] // top right
-        else if y = 0 then [| (x+1, y); (x, y+1); (x-1, y)  |] //top row
+        let right = (x+1, y)
+        let left = (x-1, y)
+        let up = (x,y-1)
+        let down = (x, y+1)
         
-        else if x = 0 && y = this.getYLength() then [| (x, y-1); (x+1, y)  |] // bottom left
-        else if x = this.getXLength() && y = this.getYLength() then [| (x-1, y); (x, y-1)  |] // bottom right
-        else if y = this.getYLength() then [| (x, y-1); (x+1, y); (x-1, y); |] //bottom row
+        if x = 0 && y = 0 then [| right; down |] // top left
+        else if x = this.getXLength() && y = 0 then [| down; left;  |] // top right
+        else if x = 0 && y = this.getYLength() then [| up; right; |] // bottom left
+        else if x = this.getXLength() && y = this.getYLength() then [| up; left; |] // bottom right
 
-        else if x = this.getXLength() then [| (x, y-1); (x-1, y); (x, y+1);  |] // right side
-        else if x = 0 then [| (x, y-1); (x+1, y); (x, y+1) |] //left side
-        else [| (x, y-1); (x+1, y); (x, y+1); (x-1, y) |]
+        else if y = 0 then [| right; down; left;  |] //top row
+        else if y = this.getYLength() then [| up; right; left; |] //bottom row
+
+        else if x = this.getXLength() then [| up; down; left; |] // right side
+        else if x = 0 then [| up; right; down |] //left side
+        else [| up; right; down; left |]
         
     member this.getLowestPoint(x:int, y:int) : Thing[]=
         let currentTile = things[y][x]
@@ -36,16 +41,11 @@ type Map(things: Thing[][])=
         if neighbours.Length = 0 then [| currentTile |]
 //        else if neighbours.Length = 1 then neighbours
         else
-            let mutable tiles :List<Thing> = []
-            for n in neighbours do
-                n
-                    |> (fun elem -> this.getLowestPoint(elem.x(), elem.y()))
-                    |> Array.toList
-                    |> List.append tiles
-                    |> ignore
-            
-            tiles |> List.toArray
-        
+            neighbours
+                |> Array.map (fun elem -> this.getLowestPoint(elem.x(), elem.y()))
+                |> Array.reduce (fun acc elem -> elem |> Array.append acc)
+                |> Array.distinct
+                
     member this.getLowestPoints() : Thing[]=
         things
             |> Array.mapi (fun y elem -> elem |> Array.mapi (fun x _ -> this.getLowestPoint(x,y)))
