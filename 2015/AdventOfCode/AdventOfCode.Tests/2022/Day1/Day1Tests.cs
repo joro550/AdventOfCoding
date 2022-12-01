39db22bad57ef824f8d2c1dac00bd5a36e0cb309
Mark <markdavies550@gmail.com>
2022-12-01 11:55:47 +0000

    Day1 2022

---
+++ 2015/AdventOfCode/AdventOfCode.Tests/2022/Day1/Day1Tests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+
+namespace AdventOfCode.Tests._2022.Day1
+{
+    public class Day1Tests
+    {
+        [Fact]
+        public void GivenAListOfCalories()
+        {
+            var calories = @"1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000";
+            var elfList =
+                AdventOfCode._2022.Day1.CaloriesCounter.Count(
+                    calories.Split(Environment.NewLine + Environment.NewLine));
+            Assert.Equal(24000, elfList.Max());
+        }
+
+        [Fact]
+        public void Puzzle1()
+        {
+            var calories = FileReader.GetResource("AdventOfCode.Tests._2022.Day1.PuzzleInput.txt");
+            var elfList =
+                AdventOfCode._2022.Day1.CaloriesCounter.Count(
+                    calories.Split(Environment.NewLine + Environment.NewLine));
+            Assert.Equal(69206, elfList.Max());
+        }
+        
+        [Fact]
+        public void Puzzle2()
+        {
+            var calories = FileReader.GetResource("AdventOfCode.Tests._2022.Day1.PuzzleInput.txt");
+            var elfList =
+                AdventOfCode._2022.Day1.CaloriesCounter.Count(
+                        calories.Split(Environment.NewLine + Environment.NewLine))
+                    .OrderDescending()
+                    .Take(3)
+                    .Sum();
+            Assert.Equal(197400, elfList);
+        }
+    }
+}
---
+++ 2015/AdventOfCode/AdventOfCode/2022/Day1/CaloriesCounter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace AdventOfCode._2022.Day1
+{
+    public class CaloriesCounter
+    {
+        public static long[] Count(IEnumerable<string> lines)
+        {
+
+            return lines.Select(line => line.Split(Environment.NewLine).Sum(long.Parse)).ToArray();
+        }
+    }
+}
