e2d083bb6a234f5e3ed6daaf3b47540c6e0cea05
joro550 <markdavies550@gmail.com>
2022-12-03 11:57:28 +0000

    Day3

---
+++ 2015/AdventOfCode/AdventOfCode.Tests/2022/Day3/Day3Tests.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using AdventOfCode._2022.Day3;
+using Xunit;
+
+namespace AdventOfCode.Tests._2022.Day3;
+
+public class Day3Tests
+{
+    [Fact]
+    public void Things()
+    {
+        var sum = ItemPriorityConverter
+            .GetSumOfSharedItems("vJrwpWtwJgWrhcsFMMfFFhFp");
+        Assert.Equal(16, sum);
+    }
+
+    [Fact]
+    public void GetOfMultipleBags()
+    {
+        var bags = @"vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw".SplitByNewLine();
+
+        var sum = 0L;
+        foreach (var bag in bags)
+        {
+            var sumOfSharedItems = ItemPriorityConverter.GetSumOfSharedItems(bag);
+            sum += sumOfSharedItems;
+        }
+        Assert.Equal(157, sum);
+    }
+
+    [Fact]
+    public void Puzzle1()
+    {
+        var input = FileReader.GetResource("2022", "3")
+            .SplitByNewLine();
+        
+        var sum = input.Sum(ItemPriorityConverter.GetSumOfSharedItems);
+        Assert.Equal(8349, sum);
+    }
+
+    [Fact]
+    public void Example2()
+    {
+        var bags = @"vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw".SplitByNewLine();
+        var sum = ItemPriorityConverter.Thing(bags);
+        Assert.Equal(70, sum);
+    }
+    
+    [Fact]
+    public void Puzzle2()
+    {
+        var input = FileReader.GetResource("2022", "3")
+            .SplitByNewLine();
+        
+        var sum = ItemPriorityConverter.Thing(input);
+        Assert.Equal(2681, sum);
+    }
+}
---
+++ 2015/AdventOfCode/AdventOfCode/2022/Day3/ItemPriorityConverter.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode._2022.Day3;
+
+public static class ItemPriorityConverter
+{
+    private static readonly Dictionary<string, long> ItemPriority
+        = GetItemPriorities();
+    private static Dictionary<string, long> GetItemPriorities()
+    {
+        const string alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        var dictionary = new Dictionary<string, long>();
+        for (var i = 0; i < alphabet.Length; i++) 
+            dictionary.Add(alphabet[i].ToString(), i+1);
+
+        return dictionary;
+    }
+
+    public static long GetSumOfSharedItems(string input)
+    {
+        var splitPoint = input.Length/ 2;
+        var firstCompartment = input[..splitPoint];
+        var secondCompartment = input[splitPoint..];
+
+        var distinct = (from item in firstCompartment
+            where secondCompartment.Contains(item)
+            select item).Distinct();
+
+        return distinct.Select(item => item.ToString())
+            .Select(itemString => ItemPriority[itemString])
+            .Sum();
+    }
+
+
+    public static long Thing(string[] lines)
+    {
+        var sum = 0L;
+        for (var i = 0; i < lines.Length; i += 3)
+        {
+            var endRange = i + 3 >= lines.Length ? lines.Length : i + 3;
+            var groupy = lines[i..endRange]
+                .ToArray();
+
+            var sharedBetween =
+                (from item in groupy[0]
+                    where groupy[1].Contains(item) && groupy[2].Contains(item)
+                    select item.ToString()).Distinct().ToList();
+
+            sum += sharedBetween.Sum(x => ItemPriority[x]);
+        }
+
+        return sum;
+    }
+}
