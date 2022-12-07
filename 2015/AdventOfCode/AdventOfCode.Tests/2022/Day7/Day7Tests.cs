ed208fc122902d63bcd4930d3a29d36cbd4b4b74
M Davies <wdavies@opentext.com>
2022-12-07 12:02:48 +0000

    Day7

---
+++ 2015/AdventOfCode/AdventOfCode.Tests/2022/Day7/Day7Tests.cs
@@ -0,0 +1,112 @@
+﻿using System.Linq;
+using AdventOfCode._2022.Day7;
+using Xunit;
+
+namespace AdventOfCode.Tests._2022.Day7;
+
+public class Day7Tests
+{
+    [Fact]
+    public void Example1()
+    {
+        const string history = @"$ cd /
+$ ls
+dir a
+14848514 b.txt
+8504156 c.dat
+dir d
+$ cd a
+$ ls
+dir e
+29116 f
+2557 g
+62596 h.lst
+$ cd e
+$ ls
+584 i
+$ cd ..
+$ cd ..
+$ cd d
+$ ls
+4060174 j
+8033020 d.log
+5626152 d.ext
+7214296 k";
+        var fileSystem = Lexer.DoThing(history);
+        var size = DirectorySizeCounter.CountSize(fileSystem);
+        var sum = size.Values.Sum();
+        Assert.Equal(95437, sum);
+    }
+    
+    [Fact]
+    public void Example2()
+    {
+        const string history = @"$ cd /
+$ ls
+dir a
+14848514 b.txt
+8504156 c.dat
+dir d
+$ cd a
+$ ls
+dir e
+29116 f
+2557 g
+62596 h.lst
+$ cd e
+$ ls
+584 i
+$ cd ..
+$ cd ..
+$ cd d
+$ ls
+4060174 j
+8033020 d.log
+5626152 d.ext
+7214296 k";
+        var fileSystem = Lexer.DoThing(history);
+        var size = DirectorySizeCounter.CountSize(fileSystem);
+        
+        const long totalDiskSize = 70000000;
+        var spaceTaken = DirectorySizeCounter.CalculateSpaceTake(fileSystem);
+        var spaceNeeded = totalDiskSize - spaceTaken;
+        
+        var sum = size.Values
+            .Where(x => x >= spaceNeeded);
+
+        var minimumSize = sum.Min();
+        Assert.Equal(24933642, minimumSize);
+    }
+    
+    [Fact]
+    public void Puzzle1()
+    {
+        string history = FileReader.GetResource("2022", "7");
+        var fileSystem = Lexer.DoThing(history);
+        var size = DirectorySizeCounter.CountSize(fileSystem);
+        
+        
+        var sum = size.Values.Where(x=> x <= 100000).Sum();
+        Assert.Equal(1845346, sum);
+    }
+    
+    [Fact]
+    public void Puzzle2()
+    {
+        string history = FileReader.GetResource("2022", "7");
+        var fileSystem = Lexer.DoThing(history);
+        var size = DirectorySizeCounter.CountSize(fileSystem);
+        
+        const long totalDiskSize = 70000000;
+        var spaceTaken = DirectorySizeCounter.CalculateSpaceTake(fileSystem);
+        var spaceLeft = totalDiskSize - spaceTaken;
+        long thing = 30000000 - spaceLeft;
+
+        
+        var sum = size.Values
+            .Where(x => x >= thing);
+
+        var minimumSize = sum.Min();
+        Assert.Equal(3636703, minimumSize);
+    }
+}
---
+++ 2015/AdventOfCode/AdventOfCode/2022/Day7/Command.cs
@@ -0,0 +1,151 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using AdventOfCode.Tests;
+
+namespace AdventOfCode._2022.Day7;
+
+public abstract class Command
+{
+    
+}
+
+public class ChangeDirectoryCommand : Command
+{
+    
+}
+
+public class ListDirectoryCommand : Command
+{
+    
+}
+
+
+public record Leaf;
+
+public record Directory(string Name, string Path,  List<Leaf> Leaves) : Leaf
+{
+    public Directory Previous { get; }
+
+    private Directory(string name, string path, Directory previous) : this(name, path, new List<Leaf>())
+    {
+        Previous = previous; 
+    }
+    public Directory(string name, string path) : this(name, path, new List<Leaf>())
+    {
+        Previous = this;
+    }
+
+    public void AddDirectory(string name)
+    {
+        var path = Path == "/" ? name : Path + $"/{name}";
+        Leaves.Add(new Directory(name, path, this));
+    }
+
+    public Directory Traverse(string path)
+    {
+        var steps = path.Split("/");
+        var directories = Leaves.OfType<Directory>().ToArray();
+
+        var directory = this;
+        return steps.Aggregate(directory, (current, step) => directories.FirstOrDefault(x => x.Name == step) ?? current);
+    }
+}
+
+public record Files(long Size, string Name) : Leaf;
+
+
+public class Lexer
+{
+    public static Directory DoThing(string history)
+    {
+        var leaves = new Directory("/", "/");
+        var currentDirectory = leaves;
+        
+        foreach (var line in history.SplitByNewLine())
+        {
+            if (line.StartsWith("$ ls"))
+                continue;
+
+            if (line.StartsWith("$ cd"))
+            {
+                currentDirectory = HandleCommand(line, leaves, currentDirectory);
+                continue;
+            }
+
+            if (line.StartsWith("dir"))
+            {
+                var splitBySpace = line.SplitBySpace();
+                currentDirectory.AddDirectory(splitBySpace[1]);
+            }
+            else
+            {
+                var lineParts = line.SplitBySpace();
+                currentDirectory.Leaves.Add(new Files(long.Parse(lineParts[0]), lineParts[1]));
+            }
+        }
+
+        return leaves;
+    }
+
+    private static Directory HandleCommand(string line, Directory directory, Directory currentDirectory)
+    {
+        var lineParts = line.SplitBySpace();
+        if (lineParts[1] != "cd") 
+            return currentDirectory;
+        
+        if (lineParts[2] == "/")
+            return directory;
+
+        if (lineParts[2] != "..")
+        {
+            var temp = currentDirectory;
+            var firstOrDefault = currentDirectory.Leaves.OfType<Directory>().FirstOrDefault(x => x.Name == lineParts[2]);
+            return firstOrDefault;
+        }
+
+        return currentDirectory.Previous;
+    }
+}
+
+public static class DirectorySizeCounter
+{
+
+    public static long CalculateSpaceTake(Directory directory)
+    { 
+        long sum = directory.Leaves.OfType<Files>().Sum(x => x.Size);
+        foreach (var directoryLeaf in directory.Leaves.OfType<Directory>())
+        {
+            sum += CountDirectorySize(directoryLeaf);
+        }
+
+        return sum;
+    }
+    
+    public static Dictionary<string, long> CountSize(Directory directory)
+    {
+        var dictionary = new Dictionary<string, long>
+        {
+            { directory.Name, CountDirectorySize(directory) }
+        };
+
+        foreach (var d in directory.Leaves.OfType<Directory>())
+        {
+            var directorySize = CountSize(d);
+            foreach (var thing in directorySize)
+            {
+                dictionary.Add($"{directory.Name}/{thing.Key}", thing.Value);
+            }
+        }
+
+        return dictionary;
+    }
+
+    private static long CountDirectorySize(Directory directory)
+    {
+        var sum =  directory.Leaves.OfType<Files>().Sum(x => x.Size);
+        var directorySize = directory.Leaves.OfType<Directory>().Sum(CountDirectorySize);
+        return sum + directorySize;
+    }
+}
